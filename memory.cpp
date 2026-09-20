1e25be2a35920bbf831e481bf59e401a93af33e3
versus3927 <aleksrod277@gmail.com>
2026-09-20 14:58:34 +0300

    Update memory.cpp

--- memory.cpp
+++ memory.cpp
@@ -1,14 +1,11 @@
+#include "offsets.h"
 
 template <typename T>
 T read_mem(int pid, uintptr_t address) {
     T value;
     std::string mem_path = "/proc/" + std::to_string(pid) + "/mem";
     std::ifstream mem_file(mem_path, std::ios::binary);
-    
-    if (!mem_file.is_open()) {
-        return T();
-    }
-
+    if (!mem_file.is_open()) return T();
     mem_file.seekg(address);
     mem_file.read(reinterpret_cast<char*>(&value), sizeof(T));
     mem_file.close();
