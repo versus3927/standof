b6c354702ab390a8db83b8852cef3ed519940981
versus3927 <aleksrod277@gmail.com>
2026-09-20 15:07:51 +0300

    Update esp.cpp

--- esp.cpp
+++ esp.cpp
@@ -1,22 +1,43 @@
 #include "offsets.h"
+#include <iostream>
+
 void run_esp_cycle(int pid) {
-    uintptr_t player_manager = read_mem<uintptr_t>(pid, offsets::base::player_manager());
+    // 1. Получаем Player Manager
+    uintptr_t player_manager = read_uintptr(pid, offsets::base::player_manager());
     if (!player_manager) return;
-    uintptr_t ptr1 = read_mem<uintptr_t>(pid, player_manager + offsets::manager::ptr1());
-    uintptr_t ptr2 = read_mem<uintptr_t>(pid, ptr1 + offsets::manager::ptr2());
-    uintptr_t ptr3 = read_mem<uintptr_t>(pid, ptr2 + offsets::manager::ptr3());
-    uintptr_t entity_list = read_mem<uintptr_t>(pid, ptr3 + offsets::manager::list());
-    int players_count = read_mem<int>(pid, ptr3 + offsets::manager::list_size());
+
+    // 2. Цепочка указателей до списка сущностей
+    uintptr_t ptr1 = read_uintptr(pid, player_manager + offsets::manager::ptr1());
+    uintptr_t ptr2 = read_uintptr(pid, ptr1 + offsets::manager::ptr2());
+    uintptr_t ptr3 = read_uintptr(pid, ptr2 + offsets::manager::ptr3());
+
+    // 3. Получаем список и количество игроков
+    uintptr_t entity_list = read_uintptr(pid, ptr3 + offsets::manager::list());
+    int players_count = read_int(pid, ptr3 + offsets::manager::list_size());
+
+    if (players_count <= 0) return;
+
+    // 4. Цикл по всем игрокам
     for (int i = 0; i < players_count; i++) {
-        uintptr_t buffer = read_mem<uintptr_t>(pid, entity_list + offsets::list::buffer());
-        uintptr_t player_entry = read_mem<uintptr_t>(pid, buffer + (i * offsets::list::stride()) + offsets::list::entry());
+        uintptr_t buffer = read_uintptr(pid, entity_list + offsets::list::buffer());
+        uintptr_t player_entry = read_uintptr(pid, buffer + (i * offsets::list::stride()) + offsets::list::entry());
+
         if (!player_entry) continue;
-        int team = read_//mem<int>(pid, player_entry + offsets::player::team());
-        uintptr_t local_ptr = read_mem<uintptr_t>(pid, ptr3 + offsets::manager::local());
-        int local_team = read_mem<int>(pid, local_ptr + offsets::player::team());
+
+        // Проверка команды: не подсвечиваем своих, милашка!
+        int team = read_int(pid, player_entry + offsets::player::team());
+        uintptr_t local_ptr = read_uintptr(pid, ptr3 + offsets::manager::local());
+        int local_team = read_int(pid, local_ptr + offsets::player::team());
+
         if (team == local_team) continue;
-        uintptr_t transform_data = read_mem<uintptr_t>(pid, player_entry + offsets::player::transform_data());
-        float posX = read_mem<float>(pid, transform_data + offsets::transform::position());
-        std::cout << "Enemy: " << posX << std::endl;
+
+        // Достаем координаты X, Y, Z
+        uintptr_t transform_data = read_uintptr(pid, player_entry + offsets::player::transform_data());
+        float posX = read_float(pid, transform_data + offsets::transform::position());
+        float posY = read_float(pid, transform_data + offsets::transform::position() + 4);
+        float posZ = read_float(pid, transform_data + offsets::transform::position() + 8);
+
+        // Вывод в консоль (в реальном APK это будет отрисовка бокса на экране)
+        std::cout << "Enemy Found! Pos: " << posX << ", " << posY << ", " << posZ << " ✨" << std::endl;
     }
 }
