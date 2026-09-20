ffe490024a92249ac8b2d8338efc6e1e4988c33f
versus3927 <aleksrod277@gmail.com>
2026-09-20 15:35:00 +0300

    Create MainActivity.kt

---
+++  app/src/main/java/com/femboy/strike/MainActivity.kt
@@ -0,0 +1,30 @@
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import java.io.*
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        val btn = Button(this).apply {
+            text = "ЗАПУСТИТЬ МАГИЮ, НЯ! ✨"
+        }
+        setContentView(btn)
+
+        btn.setOnClickListener {
+            try {
+                val process = Runtime.getRuntime().exec("su")
+                val os = DataOutputStream(process.outputStream)
+                os.writeBytes("chmod 777 /data/local/tmp/libfemboy_strike.so\n")
+                os.writeBytes("/data/local/tmp/libfemboy_strike.so &\n")
+                os.writeBytes("exit\n")
+                os.flush()
+                Toast.makeText(this, "Elite-демон запущен! Доминируй! ✨", Toast.LENGTH_LONG).show()
+            } catch (e: Exception) {
+                Toast.makeText(this, "Ошибка Root: ${e.message}", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
