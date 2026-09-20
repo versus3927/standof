7822477d1f4f35d7d6e096734c0dd522ebb2adcc
versus3927 <aleksrod277@gmail.com>
2026-09-20 15:02:42 +0300

    Update esp.cpp

--- esp.cpp
+++ esp.cpp
@@ -1,19 +1,17 @@
 #include "offsets.h"
-
 void run_esp_cycle(int pid) {
     uintptr_t player_manager = read_mem<uintptr_t>(pid, offsets::base::player_manager());
     if (!player_manager) return;
     uintptr_t ptr1 = read_mem<uintptr_t>(pid, player_manager + offsets::manager::ptr1());
     uintptr_t ptr2 = read_mem<uintptr_t>(pid, ptr1 + offsets::manager::ptr2());
     uintptr_t ptr3 = read_mem<uintptr_t>(pid, ptr2 + offsets::manager::ptr3());
-    uintptr_t entity_list = read_//mem<uintptr_t>(pid, ptr3 + offsets::manager::list());
+    uintptr_t entity_list = read_mem<uintptr_t>(pid, ptr3 + offsets::manager::list());
     int players_count = read_mem<int>(pid, ptr3 + offsets::manager::list_size());
-
     for (int i = 0; i < players_count; i++) {
         uintptr_t buffer = read_mem<uintptr_t>(pid, entity_list + offsets::list::buffer());
         uintptr_t player_entry = read_mem<uintptr_t>(pid, buffer + (i * offsets::list::stride()) + offsets::list::entry());
         if (!player_entry) continue;
-        int team = read_mem<int>(pid, player_entry + offsets::player::team());
+        int team = read_//mem<int>(pid, player_entry + offsets::player::team());
         uintptr_t local_ptr = read_mem<uintptr_t>(pid, ptr3 + offsets::manager::local());
         int local_team = read_mem<int>(pid, local_ptr + offsets::player::team());
         if (team == local_team) continue;
