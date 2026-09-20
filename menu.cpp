fe61176f6c835105fdd82f3b840c87a3ffbd4026
versus3927 <aleksrod277@gmail.com>
2026-09-20 14:59:21 +0300

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -1,42 +1,38 @@
-#include "imgui.h"  // ВОТ ЭТОА СТРОКА, КОТОРУЮ МЫ ЗАБЫЛИ!
+#include "imgui.h"
 #include "offsets.h"
+
 struct CheatConfig {
     bool esp_enabled = false;
-    bool box_esp = false;
-    bool name_esp = false;
-    bool aimbot_enabled = false;
-    float aim_fov = 90.0f;
 } config;
 
 void render_femboy_menu() {
-    ImGui::Begin("✨ Femboy-Strike External ✨");
-
-    ImGui::Text("Привет, Джек! Выбирай, что будем ломать, ня!");
-    ImGui::Separator();
-
-    if (ImGui::CollapsingHeader("Visuals (ESP)")) {
-        ImGui::Checkbox("Включить ESP", &config.esp_enabled);
-        if (config.esp_enabled) {
-            ImGui::Indent();
-            ImGui::Checkbox("Рисовать Боксы", &config.box_esp);
-            ImGui::Checkbox("Показывать Имена", &config.name_esp);
-            ImGui::Unindent();
-        }
-    }
-
-    if (ImGui::CollapsingHeader("Combat (Aimbot)")) {
-        ImGui::Checkbox("Включить Аим", &config.aimbot_enabled);
-        if (config.aimbot_enabled) {
-            ImGui::Indent();
-            ImGui::SliderFloat("FOV", &config.aim_fov, 1.0f, 180.0f);
-            ImGui::Unindent();
-        }
-    }
-
-    ImGui::Separator();
-    if (ImGui::Button("Скрыть меню (Insert)", ImVec2(200, 30))) {
-        // Логика скрытия
-    }
-
+    ImGui::Begin("✨ Femboy-Strike ✨");
+    ImGui::Checkbox("ESP", &config.esp_enabled);
     ImGui::End();
+}
+
+5. Обновляем  main.cpp :
+
+#include "offsets.h"
+#include <thread>
+#include <chrono>
+
+void esp_thread(int pid) {
+    while (true) {
+        run_esp_cycle(pid);
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+}
+
+int main() {
+    int pid = get_pid("Standoff2");
+    if (pid != -1) {
+        std::thread t(esp_thread, pid);
+        t.detach();
+    }
+    while (true) {
+        render_femboy_menu();
+        std::this_thread::sleep_for(std::chrono::milliseconds(16));
+    }
+    return 0;
 }
